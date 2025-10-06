af170c05723a31282706c2f8a35efbe4d589c064
XrystikLK <23spiisiinii@gmail.com>
2025-10-06 17:31:13 +0500

    feat: footer draft

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import '../app.css';
-	
+	import { Button } from '$lib/components/ui/button';
+	import { ClipboardClock, FileText, ShieldCheck } from '@lucide/svelte';
+	import { goto } from '$app/navigation';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="w-screen max-w-full h-screen flex flex-col">
+	{@render children()}
+	<div
+		class="w-full h-25 bg-gray-400 grid grid-cols-3 gap-5 items-center justify-center fixed bottom-0"
+	>
+		<Button class="flex flex-col py-10" onclick={() => goto('schedule')}>
+			<ClipboardClock />
+			Расписание
+		</Button>
+		<Button class="flex flex-col py-10" onclick={() => goto('documents')}>
+			<FileText />
+			Документы
+		</Button>
+		<Button class="flex flex-col py-10" onclick={() => goto('ksiva')}>
+			<ShieldCheck />
+			Ксива
+		</Button>
+	</div>
+</div>
---
+++ src/routes/ksiva/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<p>ksiva page</p>
