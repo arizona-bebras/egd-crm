4a9c5849983c4ee294389b8ff955d16d5e4c6d34
lenchq <tirr06@gmail.com>
2025-10-06 20:08:17 +0500

    feat: add tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,25 +2,30 @@
 	import '../app.css';
 	import { ClipboardClock, FileText, ShieldCheck } from '@lucide/svelte';
 	import NavbarButton from '$lib/components/ui/navbar-button/navbar-button.svelte';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 
+	const queryClient = new QueryClient();
+	
 	let { children } = $props();
 </script>
 
-<main class="sm:bg-slate-100/70">
-	<div
-		class="flex h-screen max-w-full flex-col border border-slate-300 bg-white shadow-xs sm:mx-auto sm:max-w-xl"
-	>
-		<div class="overflow-y-scroll">
-			{@render children()}
-		</div>
-		<div class="relative flex w-full flex-col">
-			<div
-				class="fixed bottom-0 grid h-fit w-full max-w-[40rem] sm:max-w-xl grid-cols-3 items-center justify-center gap-5 bg-blue-100"
-			>
-				<NavbarButton routeId="/schedule" icon={ClipboardClock} label="Расписание" />
-				<NavbarButton routeId="/documents" icon={FileText} label="Документы" />
-				<NavbarButton routeId="/ksiva" icon={ShieldCheck} label="Ксива" />
+<QueryClientProvider client={queryClient}>
+	<main class="sm:bg-slate-100/70">
+		<div
+			class="flex h-screen max-w-full flex-col border border-slate-300 bg-white shadow-xs sm:mx-auto sm:max-w-xl"
+		>
+			<div class="overflow-y-scroll">
+				{@render children()}
+			</div>
+			<div class="relative flex w-full flex-col">
+				<div
+					class="fixed bottom-0 grid h-fit w-full max-w-[40rem] grid-cols-3 items-center justify-center gap-5 bg-blue-100 sm:max-w-xl"
+				>
+					<NavbarButton routeId="/schedule" icon={ClipboardClock} label="Расписание" />
+					<NavbarButton routeId="/documents" icon={FileText} label="Документы" />
+					<NavbarButton routeId="/ksiva" icon={ShieldCheck} label="Ксива" />
+				</div>
 			</div>
 		</div>
-	</div>
-</main>
+	</main>
+</QueryClientProvider>
