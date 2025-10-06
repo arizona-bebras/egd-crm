0febbb12bc66ed1db37708fcfc75d2c95ba30a94
lenchq <tirr06@gmail.com>
2025-10-06 20:04:25 +0500

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 		</div>
 		<div class="relative flex w-full flex-col">
 			<div
-				class="fixed bottom-0 grid h-fit w-[inherit] max-w-xl grid-cols-3 items-center justify-center gap-5 bg-blue-100"
+				class="fixed bottom-0 grid h-fit w-[inherit] max-w-[40rem] grid-cols-3 items-center justify-center gap-5 bg-blue-100"
 			>
 				<NavbarButton routeId="/schedule" icon={ClipboardClock} label="Расписание" />
 				<NavbarButton routeId="/documents" icon={FileText} label="Документы" />
