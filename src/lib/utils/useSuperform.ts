d150e0cf1d3082c08c15b6cfa7ca8a5e348ff82b
lenchq <tirr06@gmail.com>
2025-10-06 22:06:56 +0500

    fix: downgrade zod@4 -> zod@3

--- src/lib/utils/useSuperform.ts
+++ src/lib/utils/useSuperform.ts
@@ -3,32 +3,32 @@ import { zod, type Infer } from 'sveltekit-superforms/adapters';
 import { defaults, superForm, type FormOptions } from 'sveltekit-superforms/client';
 import z from 'zod';
 
-export function useSuperForm<T extends z.ZodObject>(
-	schema: T,
-	options?: FormOptions<Infer<T, 'zod'>, unknown, Record<string, unknown>> & {
-		defaults?: Infer<T, 'zod'>;
-		afterSubmit?: (data: z.infer<T>) => void | Promise<void>;
-	},
+export function useSuperForm<T extends z.AnyZodObject>(
+    schema: T,
+    options?: FormOptions<Infer<T, 'zod'>, unknown, Record<string, unknown>> & {
+        defaults?: Infer<T, 'zod'>;
+        afterSubmit?: (data: z.infer<T>) => void | Promise<void>;
+    },
 ) {
-	const form = superForm(defaults(zod(schema), { defaults: options?.defaults }), {
-		SPA: true,
-		validators: zod(schema),
-		clearOnSubmit: 'none',
-		dataType: 'json',
-		async onSubmit(input) {
-			const { data, success, error } = schema.safeParse(get(form.form));
-			if (!success) {
-				logger.debug('form errors:', error);
-				return;
-			}
-			await options?.afterSubmit?.(data);
-		},
-		...options,
-	});
+    const form = superForm(defaults(zod(schema), { defaults: options?.defaults }), {
+        SPA: true,
+        validators: zod(schema),
+        clearOnSubmit: 'none',
+        dataType: 'json',
+        async onSubmit(input) {
+            const { data, success, error } = schema.safeParse(get(form.form));
+            if (!success) {
+                console.debug('form errors:', error);
+                return;
+            }
+            await options?.afterSubmit?.(data);
+        },
+        ...options,
+    });
 
-	return {
-		...form,
-		form: form,
-		formData: form.form,
-	};
+    return {
+        ...form,
+        form: form,
+        formData: form.form,
+    };
 }
