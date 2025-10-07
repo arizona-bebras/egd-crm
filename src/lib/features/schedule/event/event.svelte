c2126cdea9a3ce303ae8850a1cf235d4070c0874
shyinakira <maxi.solts@gmail.com>
2025-10-07 16:42:46 +0500

    feat: 500days

--- src/lib/features/schedule/event/event.svelte
+++ src/lib/features/schedule/event/event.svelte
@@ -2,12 +2,16 @@
     import { Dot } from '@lucide/svelte';
     let { title, startTime, endTime, venue} = $props()
     let startHours = new Date(startTime).getHours()
+    let startMinutes = new Date(startTime).getMinutes()
     let endHours = new Date(endTime).getHours()
+    let endMinutes = new Date(endTime).getMinutes()
 </script>
 <div class="flex flex-col text-[#DEDEDE] bg-accent rounded-2xl p-[16px]">
     <div class="text-xl font-bold">{title}</div>
     <div class="flex flex-row items-center justify-left max-w-80">
-        <p class="text-xl font-medium">{startHours} - {endHours}</p>
+        <p class="text-xl font-medium">
+            {startHours <= 9 ? '0' : ''}{startHours}:{startMinutes <= 9 ? '0' : ''}{startMinutes} - {endHours <= 9 ? '0' : ''}{endHours}:{endMinutes <= 9 ? '0' : ''}{endMinutes}
+        </p>
         <Dot class="w-10 h-10" />
         <p class="text-xl font-medium">{venue}</p>
     </div>
--- src/lib/widgets/schedule/day-list.svelte
+++ src/lib/widgets/schedule/day-list.svelte
@@ -1,14 +1,28 @@
-<script>
+<script lang="ts">
 	import Day from './day.svelte';
 	import VirtualList from 'svelte-tiny-virtual-list';
 
-	let data = ['2025-12-15', '2025-12-16', '2025-12-17', '2025-12-18', '2025-12-19', '2025-12-20'];
+    let { currentDate } = $props()
+
+	let data = ['2025-01-01'];
+
+    function getDays(days: string[]) {
+        for (let i = 0; i < 500; i++) {
+            const newDay = new Date(data[data.length-1]);
+            newDay.setDate(newDay.getDate() + 1);
+            data.push(newDay.toISOString().split('T')[0]);
+        }
+    }
+
+    getDays(data)
+
+
 </script>
 
 <VirtualList width="100%" height={300} itemCount={data.length} itemSize={400}>
 	{#snippet item({ style, index })}
 		<div {style}>
-			{@debug index}
+			<!-- {@debug index} -->
 			<Day date={data[index]} />
 		</div>
 	{/snippet}
