0f48290b8ad210c214884853cda8d1bcfea817f0
Emil Gusev <miloric958@gmail.com>
2025-10-07 22:10:19 +0500

    refactor(schedule): clean up day-list component and improve formatting

--- src/lib/widgets/schedule/day-list.svelte
+++ src/lib/widgets/schedule/day-list.svelte
@@ -1,46 +1,43 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Day from './day.svelte';
-    import SvelteVirtualList from '@humanspeak/svelte-virtual-list';
+	import SvelteVirtualList from '@humanspeak/svelte-virtual-list';
 
-    // let { currentDate } = $props()
-    let listRef;
+	// let { currentDate } = $props()
+	let listRef;
 	let data = ['2025-10-07'];
 
-    function getDays(days: string[]) {
-        for (let i = 1; i < 30; i++) {
-            const newDay = new Date('2025-10-07');
-            newDay.setDate(newDay.getDate() + i);
-            data.push(newDay.toISOString().split('T')[0]);
-        }
-        for (let i = 1; i < 30; i++) {
-            const newDay = new Date('2025-10-07');
-            newDay.setDate(newDay.getDate() - i);
-            data.unshift(newDay.toISOString().split('T')[0]);
-        }
-    }
+	function getDays(days: string[]) {
+		for (let i = 1; i < 30; i++) {
+			const newDay = new Date('2025-10-07');
+			newDay.setDate(newDay.getDate() + i);
+			data.push(newDay.toISOString().split('T')[0]);
+		}
+		for (let i = 1; i < 30; i++) {
+			const newDay = new Date('2025-10-07');
+			newDay.setDate(newDay.getDate() - i);
+			data.unshift(newDay.toISOString().split('T')[0]);
+		}
+	}
 
-    getDays(data)
+	getDays(data);
 
-    const items = Array.from({ length: 58 }, (_, i) => ({
-        id: i,
-        day: data[i]
-    }))
+	const items = Array.from({ length: 58 }, (_, i) => ({
+		id: i,
+		day: data[i],
+	}));
 
-
-    onMount(() => {
-	 	listRef.scroll({ index: 17, smoothScroll: true, align: 'top' });
-	 });
-
-
+	onMount(() => {
+		listRef.scroll({ index: 17, smoothScroll: true, align: 'top' });
+	});
 </script>
 
-<div class="h-[650px]">
-    <SvelteVirtualList {items} bind:this={listRef} bufferSize={30}>
-        {#snippet renderItem(item)}
-            <Day date={item.day} />
-        {/snippet}
-    </SvelteVirtualList>
+<div class="h-[650px] px-4">
+	<SvelteVirtualList {items} bind:this={listRef} bufferSize={30}>
+		{#snippet renderItem(item)}
+			<Day date={item.day} />
+		{/snippet}
+	</SvelteVirtualList>
 </div>
 <!-- 
 <div class="list">
