47926ead46ceaf9243e985bc33c6463f710e71ba
shyinakira <maxi.solts@gmail.com>
2025-10-07 21:19:52 +0500

    wip: schedule

--- src/lib/widgets/schedule/day-list.svelte
+++ src/lib/widgets/schedule/day-list.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Day from './day.svelte';
-	import VirtualList from 'svelte-tiny-virtual-list';
     import SvelteVirtualList from '@humanspeak/svelte-virtual-list';
 
     // let { currentDate } = $props()
     let listRef;
-
 	let data = ['2025-10-07'];
 
     function getDays(days: string[]) {
-        for (let i = 1; i < 100; i++) {
+        for (let i = 1; i < 30; i++) {
             const newDay = new Date('2025-10-07');
             newDay.setDate(newDay.getDate() + i);
             data.push(newDay.toISOString().split('T')[0]);
         }
-        for (let i = 1; i < 100; i++) {
+        for (let i = 1; i < 30; i++) {
             const newDay = new Date('2025-10-07');
             newDay.setDate(newDay.getDate() - i);
             data.unshift(newDay.toISOString().split('T')[0]);
@@ -24,21 +22,21 @@
 
     getDays(data)
 
-    const items = Array.from({ length: 200 }, (_, i) => ({
+    const items = Array.from({ length: 58 }, (_, i) => ({
         id: i,
         day: data[i]
     }))
 
 
     onMount(() => {
-	 	listRef.scroll({ index: 73, smoothScroll: true, align: 'bottom' });
+	 	listRef.scroll({ index: 17, smoothScroll: true, align: 'top' });
 	 });
 
 
 </script>
 
-<div class="list h-[600px]">
-    <SvelteVirtualList {items} bind:this={listRef} mode="topToBottom">
+<div class="h-[650px]">
+    <SvelteVirtualList {items} bind:this={listRef} bufferSize={30}>
         {#snippet renderItem(item)}
             <Day date={item.day} />
         {/snippet}
