f3fb97beadfc9595a60724ac35bf6c0cb4545f46
XrystikLK <23spiisiinii@gmail.com>
2025-10-08 14:33:59 +0500

    feat(auth): validation, design

--- src/lib/features/login-form/schema.ts
+++ src/lib/features/login-form/schema.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-	email: z.string().min(2).max(50),
-	password: z.string().min(2).max(50),
+	email: z.string().email({ message: 'Неверный формат email' }),
+	password: z
+		.string()
+		.min(2, { message: 'Длина пароля должна быть более 1 символа' })
+		.max(50, { message: 'Длина пароля должна быть не более 50 символов' }),
 });
 
 export type LoginSchema = typeof loginSchema;
