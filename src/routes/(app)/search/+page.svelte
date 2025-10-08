1287340671e943d90b74702e97b6077facde4fa9
shyinakira <maxi.solts@gmail.com>
2025-10-08 16:26:47 +0500

    feat: catalog_items_search

--- src/routes/(app)/search/+page.svelte
+++ src/routes/(app)/search/+page.svelte
@@ -5,6 +5,7 @@
 	import { supabase } from '$lib/supabaseClient';
 	import ContactItem from '$lib/widgets/contact-list/contact-item.svelte';
 	import DocumentCatalogCard from '$lib/widgets/documents/document-catalog-card.svelte';
+	import CategoryItem from '$lib/features/documents/category-item.svelte';
 	import { ChevronLeft, Search } from '@lucide/svelte';
 	import { createQuery } from '@tanstack/svelte-query';
 	import { Debounced } from 'runed';
@@ -16,6 +17,11 @@
 		queryKey: ['catalogs', searchQuery.current],
 		enabled: () => searchQuery.current.length > 3,
 		queryFn: () => supabase.rpc('search_catalogs', { prefix: searchQuery.current }),
+	}));
+	const catalogItemsSearch = createQuery(() => ({
+		queryKey: ['profiles', searchQuery.current],
+		enabled: () => searchQuery.current.length > 3,
+		queryFn: () => supabase.rpc('search_catalog_items_by_title', { prefix: searchQuery.current }),
 	}));
 	const profilesSearch = createQuery(() => ({
 		queryKey: ['profiles', searchQuery.current],
@@ -52,7 +58,7 @@
 		</div>
 		{#if searchQuery.current.length > 3}
 			<div class="mt-4">
-				<span class="text-accent font-bold">Контакты</span>
+				<span class="text-accent font-bold mt-3">Контакты</span>
 				<div class="flex flex-col gap-y-1">
 					<svelte:boundary>
 						{#each (await profilesSearch.promise).data ?? [] as profile (profile.user_id)}
@@ -71,9 +77,27 @@
 				<span class="text-accent font-bold">Каталоги</span>
 				<div class="flex flex-1 flex-wrap">
 					<svelte:boundary>
-						<div class="mb-4 grid grid-cols-2 gap-2.5">
+						<div class="mb-4 grid grid-cols-2 gap-2.5 mt-3">
 							{#each (await catalogsSearch.promise).data ?? [] as catalog (catalog.id)}
 								<DocumentCatalogCard title={catalog.title} />
+							{:else}
+								<span class="text-accent">Ничего не найдено</span>
+							{/each}
+						</div>
+						{#snippet pending()}
+							<Skeleton class="m-2 aspect-square w-[calc(50%-2rem)]" />
+							<Skeleton class="m-2 aspect-square w-[calc(50%-2rem)]" />
+						{/snippet}
+					</svelte:boundary>
+				</div>
+			</div>
+			<div class="mt-4">
+				<span class="text-accent font-bold">Разделы каталога</span>
+				<div class="flex flex-1 flex-wrap">
+					<svelte:boundary>
+						<div class="mb-4 grid grid-cols-1 gap-2.5 mt-3">
+							{#each (await catalogItemsSearch.promise).data ?? [] as catalog}
+								<CategoryItem title={catalog.title} redirect_link={catalog.redirect_link} document_link={catalog.document_link}/>
 							{:else}
 								<span class="text-accent">Ничего не найдено</span>
 							{/each}
