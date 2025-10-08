5f88996ef6871105fc1b8f47742590db3aaad456
shyinakira <maxi.solts@gmail.com>
2025-10-08 14:52:00 +0500

    wip: meetings

---
+++ src/routes/(app)/schedule/meeting/[meeting_id]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { ChevronRight } from '@lucide/svelte';
+	import { createQuery } from '@tanstack/svelte-query';
+	import { supabase } from '$lib/supabaseClient';
+    let { data } = $props();
+    const event = createQuery(() => ({
+		queryKey: ['catalog', data.route],
+		queryFn: () => supabase.from('events').select('').eq('id', data.route),
+	}));
+</script>
